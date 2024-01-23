40be05d49b1aa0e44f8ef218ef96a431ff4d838d
Diego Nunes <diegodnunes12@gmail.com>
2024-01-23 19:08:40 -0300

    criando estrutura do projeto

---
+++ src/app/[postId]/page.tsx
@@ -0,0 +1,13 @@
+import styles from "../page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Desenvolvimento de sites com Next</h1>
+
+      <div>
+          <p>Hoje vamos desenvolver um site bem legal utlizando o nextjs. Não perca!</p>
+      </div>
+    </main>
+  );
+}
